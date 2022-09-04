793a4f40dfae056b207dede8c56139249506e74b
Kimia Sameti <36294855+kimiasmt@users.noreply.github.com>
2022-09-04 17:45:29 +0430

    service types

---
+++ src/app/pages/server-list/config/serviceTypes..ts
@@ -0,0 +1,24 @@
+export const serviceTypes = [
+{type: "CDN",
+  icon: "'../../../../../assets/icons/CDN.svg'"
+},
+  {type: "Hosting",
+  icon: "'../../../../../assets/icons/Hosting.svg'"
+},
+  {type: "SSl",
+  icon: "'../../../../../assets/icons/SSl.svg'"
+},
+  {type: "Server",
+  icon: "'../../../../../assets/icons/Server.svg'"
+},
+  {type: "Domain",
+  icon: "'../../../../../assets/icons/Domain.svg'"
+},
+  {type: "Other",
+  icon: "'../../../../../assets/icons/other-services.svg'"
+},
+  {type: "All",
+  icon: "'../../../../../assets/icons/all-services.svg'"
+},
+
+]
